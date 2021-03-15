f97d497a659d06bf8bf360f83a296146abf5c9d5
OMEGONCIO <jozue25200al@gmail.com>
2021-03-14 21:23:23 -0600

    test finished

--- Prueba_2/Prueba2 _Sentencias SQL.sql
+++ Prueba_2/Prueba2 _Sentencias SQL.sql
@@ -14,8 +14,25 @@ tipoDeLlamada varchar(15) not null,
 Foreign Key(tipoDeLlamada) references costos(tipoDeLLamada)
 );
 
+INSERT INTO costos(tipoDeLlamada, costo)
+VALUES ("Cel", 0.4),
+       ("LD nacional", 0.12),
+			 ("Cel LD", 0.5);
+			 
+INSERT INTO logDial(idLlamada, fechaDeLlamada, tiempoDialogo, telefono, tipoDeLlamada)
+VALUES ("197463",'2020-01-01 00:00:00',60,"8000000001","Cel LD"),
+			 ("197464","2020-01-02 01:26:45",60,"8000000002","Cel LD"),
+			 ("197465","2020-02-03 01:26:45",240,"8000000003","Cel LD"),
+			 ("197466","2020-02-04 01:26:45",60,"8000000004","Cel LD"),
+			 ("197467","2020-02-05 01:26:45",60,"8000000005","Cel LD"),
+			 ("197468","2020-02-06 01:26:45",60,"8000000006","Cel LD"),
+			 ("197469","2020-01-07 01:26:45",60,"8000000007","Cel"),
+			 ("197470","2020-01-08 01:26:45",120,"8000000008","LD nacional"),
+			 ("197471","2020-01-09 01:26:45",60,"8000000009","Cel LD"),
+			 ("197472","2020-01-10 01:26:45",60,"8000000010","Cel LD");
+
 select * from logDial where tipoDeLlamada = 'Cel LD' and month(fechaDeLlamada)=2;
 
 select AVG(tiempoDialogo) from logDial where tipoDeLlamada = 'Cel LD' and month(fechaDeLlamada)=2;
 
-select TIME_TO_SEC(logDial.tiempoDialogo)/60, costos.costo * logDial.tiempoDialogo/60 from logDial, costos where month(fechaDeLlamada)=1;
+select logDial.tiempoDialogo/60 as tiempoDialogoEnMinutos, costos.costo * logDial.tiempoDialogo/60 as CostoLlamada from logDial Left Join costos on logDial.TipoDeLlamada = costos.TipoDeLlamada where month(fechaDeLlamada)=1;
